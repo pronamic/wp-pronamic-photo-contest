e542f10be7934cf023c4d2fd6940e1a80b158bad
Remco Tolsma <info@remcotolsma.nl>
2013-09-26 11:48:45 +0200

    Added photo contest post type.

--- pronamic-photo-contest.php
+++ pronamic-photo-contest.php
@@ -53,6 +53,33 @@ class Pronamic_WP_PhotoContestPlugin {
 	 * Initialize
 	 */
 	public function init() {
+		register_post_type( 'pronamic_photo_conte', array(
+			'labels'             => array(
+				'name'               => _x( 'Photo Contests', 'post type general name', 'pronamic_photo_contest' ),
+				'singular_name'      => _x( 'Photo Contest', 'post type singular name', 'pronamic_photo_contest' ),
+				'add_new'            => _x( 'Add New', 'pronamic_photo_contest', 'pronamic_photo_contest' ),
+				'add_new_item'       => __( 'Add New Photo Contest', 'pronamic_photo_contest' ),
+				'edit_item'          => __( 'Edit Photo Contest', 'pronamic_photo_contest' ),
+				'new_item'           => __( 'New Photo Contest', 'pronamic_photo_contest' ),
+				'view_item'          => __( 'View Photo Contest', 'pronamic_photo_contest' ),
+				'search_items'       => __( 'Search Photo Contests', 'pronamic_photo_contest' ),
+				'not_found'          => __( 'No photo contests found', 'pronamic_photo_contest' ),
+				'not_found_in_trash' => __( 'No photo contests found in Trash', 'pronamic_photo_contest' ),
+				'parent_item_colon'  => __( 'Parent Photo Contest:', 'pronamic_photo_contest' ),
+				'menu_name'          => __( 'Photo Contests', 'pronamic_photo_contest' )
+			) ,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'rewrite'            => array( 'slug' => _x( 'photo-contests', 'URL slug', 'pronamic_photo_contest' ) ),
+			'menu_icon'          => plugins_url( 'admin/images/camera.png', $this->file ),
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields' )
+		) );
+
 		register_post_type( 'pronamic_photo_entry', array(
 			'labels'             => array(
 				'name'               => _x( 'Photo Entries', 'post type general name', 'pronamic_photo_contest' ),
@@ -66,38 +93,18 @@ class Pronamic_WP_PhotoContestPlugin {
 				'not_found'          => __( 'No photo entries found', 'pronamic_photo_contest' ),
 				'not_found_in_trash' => __( 'No photo entries found in Trash', 'pronamic_photo_contest' ),
 				'parent_item_colon'  => __( 'Parent Photo Entry:', 'pronamic_photo_contest' ),
-				'menu_name'          => __( 'Photo Entries', 'pronamic_photo_contest' )
+				'menu_name'          => __( 'Entries', 'pronamic_photo_contest' )
 			) ,
 			'public'             => true,
 			'publicly_queryable' => true,
 			'show_ui'            => true,
-			'show_in_menu'       => true,
+			'show_in_menu'       => 'edit.php?post_type=pronamic_photo_conte',
 			'query_var'          => true,
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'rewrite'            => array( 'slug' => _x( 'photo-entries', 'URL slug', 'pronamic_photo_contest' ) ),
 			'menu_icon'          => plugins_url( 'admin/images/camera.png', $this->file ),
 			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields' )
-		) );
-		
-		register_taxonomy( 'pronamic_photo_contest', 'pronamic_photo_entry', array(
-			'hierarchical' => true ,
-			'labels' => array(
-				'name' => _x( 'Photo Contests', 'category general name', 'pronamic_photo_contest' ),
-				'singular_name' => _x( 'Photo Contest', 'category singular name', 'pronamic_photo_contest' ),
-				'search_items' => __( 'Search Photo Contests', 'pronamic_photo_contest' ),
-				'all_items' => __( 'All Photo Contests', 'pronamic_photo_contest' ),
-				'parent_item' => __( 'Parent Photo Contest', 'pronamic_photo_contest' ),
-				'parent_item_colon' => __( 'Parent Photo Contest:', 'pronamic_photo_contest' ),
-				'edit_item' => __( 'Edit Photo Contest', 'pronamic_photo_contest' ),
-				'update_item' => __( 'Update Photo Contest', 'pronamic_photo_contest' ),
-				'add_new_item' => __( 'Add New Photo Contest', 'pronamic_photo_contest' ),
-				'new_item_name' => __( 'New Photo Contest Name', 'pronamic_photo_contest' ),
-				'menu_name' => __( 'Photo Contests', 'pronamic_photo_contest' )
-			),
-			'show_ui' => true ,
-			'query_var' => true ,
-			'rewrite' => array( 'slug' => _x( 'photo-contest', 'URL slug', 'pronamic_photo_contest' ) ),
 		) );
 	}
 }
